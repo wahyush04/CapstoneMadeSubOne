3819b6be544794e8b43409731255c70b42989c9a
wahyush04 <wahyush04@gmail.com>
2022-11-12 20:46:33 +0800

    first commit

---
+++ app/src/main/java/com/wahyush04/capstonemadesubone/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.wahyush04.capstonemadesubone.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wahyush04.capstonemadesubone.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/wahyush04/capstonemadesubone/ui/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,26 @@
+package com.wahyush04.capstonemadesubone.ui.splashscreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.wahyush04.capstonemadesubone.R
+import com.wahyush04.capstonemadesubone.databinding.ActivitySplashScreenBinding
+import com.wahyush04.capstonemadesubone.ui.main.MainActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar?.hide()
+
+//        Handler(mainLooper).postDelayed({
+//            startActivity(Intent(this, MainActivity::class.java)).also {
+//                finish()
+//            }
+//        }, SPLASH_SCREEN_DELAY.toLong())
+    }
+}
