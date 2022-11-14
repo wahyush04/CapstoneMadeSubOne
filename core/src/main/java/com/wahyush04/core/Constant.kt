0d263242855aeceb1e3432acc80f830a66c56651
wahyush04 <wahyush04@gmail.com>
2022-11-14 22:31:25 +0800

    fix bug gradle

--- core/src/main/java/com/wahyush04/core/Constant.kt
+++ core/src/main/java/com/wahyush04/core/Constant.kt
@@ -1,20 +1,9 @@
 package com.wahyush04.core
 
 object Constant {
-
     const val SPLASH_SCREEN_DELAY = 3_000
-    const val ITEMS_POSTER_RADIUS = 18
-    const val DETAILS_POSTER_RADIUS = 36
-    const val RADIUS = 3
-    const val SAMPLING = 6
-    const val TIME_OUT = 120
     const val THREAD_COUNT = 3
     const val API_KEY = "2bf4768668d0c1b52884741695e57f51"
-
     const val BASE_URL = "https://gnews.io/api/v4/"
     const val BOOKMARK_URI = "gnews://bookmark"
-    const val DATABASE_NAME = "gnews"
-    const val DATABASE = "gnews.db"
-    const val TAG = "RemoteDataSource"
-
 }
